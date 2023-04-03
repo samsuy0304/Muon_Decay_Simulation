75595cbf14a2ce487f80aabccba958b4f8e73ccc
samsuy0304 <71359041+samsuy0304@users.noreply.github.com>
2023-04-03 18:40:47 -0500

    Updated_Parameters

--- Parameter_Estimation.py
+++ Parameter_Estimation.py
@@ -11,34 +11,6 @@ import sys
 def Average(lst):
     return sum(lst) / len(lst)
 
-def BubbleSort(array):
-    n = len(array)
-
-    for i in range(n):
-        # Create a flag that will allow the function to
-        # terminate early if there's nothing left to sort
-        already_sorted = True
-
-        # Start looking at each item of the list one by one,
-        # comparing it with its adjacent value. With each
-        # iteration, the portion of the array that you look at
-        # shrinks because the remaining items have already been
-        # sorted.
-        for j in range(n - i - 1):
-            if array[j] > array[j + 1]:
-                # If the item you're looking at is greater than its
-                # adjacent value, then swap them
-                array[j], array[j + 1] = array[j + 1], array[j]
-
-                # Since you had to swap two elements,
-                # set the `already_sorted` flag to `False` so the
-                # algorithm doesn't finish prematurely
-                already_sorted = False
-
-        # If there were no swaps during the last iteration,
-        # the array is already sorted, and you can terminate
-        if already_sorted:
-            break
 def CreateLabels(m,d):
     n = m.split("/")
     y = n[1].split("_")
@@ -87,7 +59,7 @@ if __name__ == "__main__":
     
     if '-meas' in sys.argv:
         index = sys.argv.index('-meas')
-        InputFile1 = int(sys.argv[index + 1])
+        Nmeas = int(sys.argv[index + 1])
 
     if '-I1' in sys.argv:
         index = sys.argv.index('-I1')
